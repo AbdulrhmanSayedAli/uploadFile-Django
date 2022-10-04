aa2523158853d2cc74e4063981c649a51f50c8bd
AbdulrhmanSayedAli <roctes7@gmail.com>
2022-10-04 14:39:12 +0300

    finishing upload end point
    1- created view with only post method to upload an image.
    2- created serilaizer class to validate & save image.

--- images/views.py
+++ images/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser
+from .serilaizer import ImageSerializer
 
 
 class Image (APIView):
-    def get(self, request, *args, **kwargs):
-        return Response(data={"r": "hi"})
+    parser_classes = [FileUploadParser]
+
+    def post(self, request, *args, **kwargs):
+        file = request.data['file']
+        serilaizer = ImageSerializer(data={**request.data, "image": file})
+        if serilaizer.is_valid():
+            serilaizer.save()
+            return Response(serilaizer.data)
+        return Response(serilaizer.errors)
