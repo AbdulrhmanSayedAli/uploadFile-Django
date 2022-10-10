0ebaa76b5f75aa07660a1e9098ef58d2522d9644
AbdulrhmanSayedAli <roctes7@gmail.com>
2022-10-10 15:09:58 +0300

    Adding a test heavy task
    1- changed the url from images/upload to images/
    2- added another endpoint [GET] to get all images
    3- made the GET request takes 10 seconds to load

--- images/urls.py
+++ images/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Image
 
 urlpatterns = [
-    path('upload/', Image.as_view()),
+    path('', Image.as_view()),
 ]
--- images/views.py
+++ images/views.py
@@ -1,8 +1,10 @@
+import time
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import FileUploadParser
 from .serilaizer import ImageSerializer
+from .models import Image as ImageModel
 
 
 class Image (APIView):
@@ -15,3 +17,8 @@ class Image (APIView):
             serilaizer.save()
             return Response(serilaizer.data)
         return Response(serilaizer.errors)
+
+    def get(self, request, *args, **kwargs):
+        serilaizer = ImageSerializer(ImageModel.objects, many=True)
+        time.sleep(10)
+        return Response(serilaizer.data)
